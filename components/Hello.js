d060b3fda2256dad4c9e523d4a591f50d2e02f31
Thao Le <thao.le@s3corp.com.vn>
2019-11-07 17:38:54 +0700

    first push

---
+++ components/Hello.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native'; 
+
+export default class Hello extends Component {
+    render() {
+        return (
+            <Text>Hello, this is my first Component</Text>
+        );
+    }
+}
